b06f19f147da4302b9d7d1a59bc55076d5ec074b
InakiHDF <gongorainaki@gmail.com>
2025-11-26 23:43:54 -0300

    fix: correct font className template

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className={${fontSans.variable}  antialiased}>
+      <body className={`${fontSans.variable} ${fontMono.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
